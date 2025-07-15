836ebe411a315d8931649ebc130e982e68627376
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-07-15 21:07:14 +0300

    1507 2107

---
+++ lib/api.ts
@@ -0,0 +1,18 @@
+export type LoginRequest = {
+  email: string;
+  password: string;
+};
+
+export const login = async (data: LoginRequest) => {
+  const res = await nextServer.post<User>('/auth/login', data);
+  return res.data;
+};
+
+type CheckSessionRequest = {
+  success: boolean;
+};
+
+export const checkSession = async () => {
+  const res = await nextServer.get<CheckSessionRequest>('/auth/session');
+  return res.data.success;
+};

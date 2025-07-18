9c0a60fcf8baab9e350be0dbd27633e48d2a80bc
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-07-18 10:55:59 +0300

    1807 1055

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 export default function UpPageLayout({ children }: { children: ReactNode }) {
+    const [loading, setLoading] = useState(true);
     const router = useRouter();
     useEffect(() => {
         router.refresh();
+        setLoading(false);
     }, [router]);
     return children;
 }

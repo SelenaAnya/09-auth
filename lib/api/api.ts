4f7343d8c7a12db6361b3fbd8d52cd9a3e6b48a1
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-07-20 16:06:49 +0300

    2007 1606

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const nextServer = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
+  // baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
 

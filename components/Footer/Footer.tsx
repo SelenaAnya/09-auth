d159c2a7b29f40ba2d2ca8bca49bae78b023aa29
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-07-20 17:05:29 +0300

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: </p>
+          <p>Developer: Anna Kovalenko</p>
           <p>
             Contact us:{" "}
             <a href="mailto: selena.anya@gmail.com">

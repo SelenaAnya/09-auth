404ad1f60078c64ca4e9a3413c2ab2dd630bac34
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-07-20 18:24:54 +0300

    2007 1824

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,13 @@
-export default function Loading() {
-  return <p>Loading, please wait...</p>;
+import css from "./Loader.module.css";
+
+function Loader() {
+  return (
+    <>
+      <div className={css.loaderFour} >
+        <div className={css.loaderFour__preloader} ></div>
+      </div>
+    </>
+  );
 }
+
+export default Loader;
